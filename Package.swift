9fa625beb2a1dd4c91d107fa1343c27f46442041
이준영 <junlight94@gmail.com>
2023-08-27 14:02:26 +0900

    라이브러리 배포

--- Package.swift
+++ Package.swift
@@ -5,24 +5,30 @@ import PackageDescription
 
 let package = Package(
     name: "DesignSystem",
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DesignSystem",
             targets: ["DesignSystem"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/layoutBox/FlexLayout.git", exact: "1.3.18"),
+        .package(url: "https://github.com/layoutBox/PinLayout.git", exact: "1.10.4")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DesignSystem",
-            dependencies: []),
-        .testTarget(
-            name: "DesignSystemTests",
-            dependencies: ["DesignSystem"]),
+            dependencies: [
+                "FlexLayout",
+                "PinLayout"
+            ],cSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE")
+            ],
+            cxxSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE"),
+            ],
+            swiftSettings: [
+                .define("FLEXLAYOUT_SWIFT_PACKAGE")
+            ])
     ]
 )
